107b0bea1c103899752d88f713ed3ce270e63df8
Gareth Dsouza <gareth.dsouza@clover.com>
2020-12-14 21:39:38 -0500

    Add auth

--- packages/auth/src/bootstrap.js
+++ packages/auth/src/bootstrap.js
@@ -27,7 +27,7 @@ const mount = (element, { onNavigate, defaultHistory, initialPath }) => {
 
 // If we are in development and isolation call mount immediately
 if (process.env.NODE_ENV === 'development') {
-  const devRoot = document.getElementById('_marketing-dev-root');
+  const devRoot = document.getElementById('_auth-dev-root');
 
   if (devRoot) {
     mount(devRoot, { defaultHistory: createBrowserHistory() });
